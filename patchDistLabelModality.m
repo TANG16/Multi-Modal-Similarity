7e3a50e1830a8f808ede18abaecde64038379270
vishalbhalla <vishal.a.bhalla@gmail.com>
2015-05-06 19:31:41 +0200

    Added code for Clustering by fast search-and-find of density peaks.

--- patchDistLabelModality.m
+++ patchDistLabelModality.m
@@ -14,6 +14,25 @@
 % Step 3b: Plot the patch by patch co-relations on a Distance Matrix.
 
 % Step 4: Compute the clustering according to the idea proposed in the paper using the Distance matrix computed above.
+% Step 4a: Compute the Density matrix using
+%          dc as some cut-off or threshold value.
+%          For all points di in one row across all coulmns j of the distance matrix.
+%          X(dij - dc)=1 if (dij - dc)<0 and X(dij - dc)=0 if(dij - dc)>0
+%          Take the sum which is the density for point i.
+
+% Step 4b: Compute the Delta matrix using
+%          Minimum distance between point i and any other point with higher density value.
+%          Take all points having density greater than the current point and take the least one out of them.
+
+% Step 4c: Plot Density as X-Axis and Delta as Y-Axis matrices.
+%          Select points with higher density and delta values as cluster centroids.
+
+%% Above step 4 already implemented in paper in the file cluster_dp.m
+% Use it by wrapping a function around it.
+
+
+% Step 4d: Plot Density as X-Axis and Delta as Y-Axis matrices.
+%          Select points with higher density and delta values as cluster centroids.
 
 % Step 5: Label the patches in the same modality using the cluster centroids.
 
