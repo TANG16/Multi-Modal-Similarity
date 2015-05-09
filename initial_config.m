1364f8fa50df23480b23a7f27c614ad3f7c5187e
vishalbhalla <vishal.a.bhalla@gmail.com>
2015-05-09 09:21:43 +0200

    Added initial_config func to initialize locally the file name, path & patch size.

---
+++ initial_config.m
@@ -0,0 +1,14 @@
+function [file, pathFile, patchSize] = initial_config()
+%% initial_config function will initialize all the variables like file name, path & patch size.
+%   Keep this file local and not check in as it has machine specific configurations.
+
+path = strcat(pwd,'/Multi-Modal-Similarity/Dataset/');
+path = strcat(pwd,'\Dataset\');
+file = 'T1_01'; % Change the filename to pick corresponding .TIFF file and it will generate its distance matrix as .dat file
+ext = '.TIFF';
+fname = strcat(file, ext);
+pathFile = strcat(path, fname);
+patchSize = 8;
+
+end
+
